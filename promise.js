280a115646d7b65cb143e4ac31ebb258cfd22645
anggawiguna1 <angga065109205@gmail.com>
2022-06-12 10:36:01 +0700

    add async

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    let dataIXX = await promiseTheaterIXX();
+    let dataVGC = await promiseTheaterVGC();
+    let dataTotal = dataIXX.concat(dataVGC);
+    return dataTotal.filter((element) => element.hasil === emosi).length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
